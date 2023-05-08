6b3001f4a00b13ce83822429541ee663bc54e56b
darker <darkerros@yandex.ru>
2023-05-08 20:50:59 +0300

    feat: add form data interface and add export

---
+++ src/forms/booking-conference-room-form/index.ts
@@ -0,0 +1,2 @@
+export {BookingConferenceRoomForm} from "./components/bookink-conference-room-form/booking-conference-room-form";
+export type {IFormData} from "./types/form-data.interface";
---
+++ src/forms/booking-conference-room-form/types/form-data.interface.ts
@@ -0,0 +1,8 @@
+export interface IFormData {
+    towerName: string;
+    stage: string;
+    room: string;
+    date: string;
+    time: string;
+    comment: string;
+}
