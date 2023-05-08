d20806f50bcd5109787de2f3792c29805009873e
darker <darkerros@yandex.ru>
2023-05-08 20:51:31 +0300

    feat: add handle submit to app

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 import styles from './app.module.scss'
 import {
   BookingConferenceRoomForm
-} from "../forms/booking-conference-room-form/components/bookink-conference-room-form/booking-conference-room-form";
+} from "../forms/booking-conference-room-form";
+import {IFormData} from "../forms/booking-conference-room-form";
 
 function App() {
+
+    const handleFormSubmit = useCallback((formData: IFormData) => {
+        console.log(JSON.stringify(formData))
+    },[])
+
   return (
     <div className={styles.app}>
         <main>
-            <BookingConferenceRoomForm/>
+            <BookingConferenceRoomForm onSubmit={handleFormSubmit}/>
         </main>
     </div>
   );
