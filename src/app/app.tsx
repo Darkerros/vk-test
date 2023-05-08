954979866e47547bb0b7cde6973001fc2690b2af
darker <darkerros@yandex.ru>
2023-05-08 20:44:47 +0300

    feat: add form to app

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import styles from './app.module.scss'
+import {
+  BookingConferenceRoomForm
+} from "../forms/booking-conference-room-form/components/bookink-conference-room-form/booking-conference-room-form";
 
 function App() {
   return (
     <div className={styles.app}>
-
+        <main>
+            <BookingConferenceRoomForm/>
+        </main>
     </div>
   );
 }
